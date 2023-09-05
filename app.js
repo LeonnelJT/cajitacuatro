c17eccedfc1dcdcb5953a4bb49d24be94922b4a3
LeonnelJT <yunglett4Lfe_2323@outlook.com>
2023-09-05 00:54:30 -0300

    se arreglo app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 let app = 23
-<<<<<<< HEAD
-let colores = [1,2,4,2,4,"rojo","azul","amarillo"];
-let steve={
+let colores = [1,2,4,2,4,"rojo","azul","amarillo", 44, 222, 0];
+let estiv={
     nombre: "alex",
     mundo: "minecraft",
     obejtivo:"matar al dragon",
     hambre: "mucha"
 }
-=======
-let colores = [44, 222, 0];
+
 let autos = ["Paris", "Argentina"]
->>>>>>> 808267178d22694a91405071c64e8f78956ac977
