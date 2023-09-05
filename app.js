bceb919aae2f041d55640f605c39e79522b2030f
rhovelazquez <rocyvelazquez2021@gmail.com>
2023-09-05 00:47:52 -0300

    se modifico app.js agregando un objeto}

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 let app = 23
-let colores = [1,2,4,2,4,];
+let colores = [1,2,4,2,4,"rojo","azul","amarillo"];
+let steve={
+    nombre: "alex",
+    mundo: "minecraft",
+    obejtivo:"matar al dragon",
+    hambre: "mucha"
+}
